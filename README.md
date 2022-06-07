c9be726b206041c3f347ad006a12b342f890bb25
bigpug <101833648+BigPugLabs@users.noreply.github.com>
2022-06-07 22:16:23 +0000

    add REST plugin for testing POST methods

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # fso-node-express
+
+[https://fullstackopen.com/en/part3/node_js_and_express](https://fullstackopen.com/en/part3/node_js_and_express)
+
+
+### Install
+
+### Restful sample app
+
+routes
+- `http://localhost:3001/`
+- `http://localhost:3001/api/notes/`
+- `http://localhost:3001/api/notes/` followed by id number
+
+### VSCode Rest client extension
+
+[https://marketplace.visualstudio.com/items?itemName=humao.rest-client](https://marketplace.visualstudio.com/items?itemName=humao.rest-client)
+
+The files in the requests folder are intended to be used with this plugin
+for VS Code
--- index.js
+++ index.js
@@ -22,8 +22,19 @@ let notes = [
     }
 ]
 
+const generateId = () => {
+    const maxId = notes.length > 0 ? Math.max(...notes.map(e=>e.id)) : 0
+    return maxId + 1
+}
+
+app.use(express.json())
+
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World</h1}')
+    response.send('<h1>Hello World</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
 })
 
 app.get('/api/notes/:id', (request, response) => {
@@ -34,6 +45,31 @@ app.get('/api/notes/:id', (request, response) => {
     } else {
         response.status(404).end()
     }
+})
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId()
+    }
+
+    notes = notes.concat(note)
+    response.json(note)
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note=> note.id !== id)
+    response.status(204).end()
 })
 
 const PORT = 3001
